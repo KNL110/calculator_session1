d639da8f8e4f6b2f3b109395c212cadc31937377
KNL110 <asarikrunal9@gmail.com>
2025-10-04 16:41:57 +0530

    style: update key event handling for calculator and add clear functionality

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let lastInputisOperator = false;
 
 document.addEventListener("keydown",(event) => {
     const key = event.key;
+    console.log(key);
+    
     if(!isNaN(key) || key === "."){
         appendnumber(key);
     } else if(['+','-','*','/','%'].includes(key)){
@@ -15,7 +17,7 @@ document.addEventListener("keydown",(event) => {
         calculate();
     } else if(key === "Backspace" || key === "DEL"){
         deletelast();
-    } else if(key === "AC"){
+    } else if(key === "Escape"){
         clearDisplay();
     }
 } );
@@ -23,16 +25,31 @@ document.addEventListener("keydown",(event) => {
 function appendnumber(key){
     // if last input was operator, clear the main display
     if(lastInputisOperator){
-        dislpay.value = "";
+        dislpay.innerHTML = "";
         lastInputisOperator = false;
     }
 
-    dislpay.value += key;
+    currExp += key;
+    dislpay.innerHTML += key;
 }
 
 function appendOperator(key){
-    if(dislpay.value === '' || currExp === '') return;
-
-    currExp = dislpay.value;
-    expressionDisplay += currExp + ' ' + `${key}`;
+    if(dislpay.innerHTML === '' || currExp === '') return;
+    expressionDisplay.innerHTML = currExp + ' ' + `${key}`;
+    currExp += key;
+    dislpay.innerHTML = "";
 }
+   
+function deletelast(){
+    n = currExp.length;
+    currExp = currExp.substring(0,n-1);
+    dislpay.innerHTML = dislpay.innerHTML.slice(0,-1);
+}
+
+function clearDisplay(){
+    dislpay.innerHTML = '';
+    currExp = '';
+    expressionDisplay.innerHTML = "";
+    lastInputisOperator = false;
+}
+
